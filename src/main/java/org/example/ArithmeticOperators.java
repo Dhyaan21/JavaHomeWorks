d2d819e527541092459f816a1456eb6b3c241bc1
Smita Patel <smita.patel@example.com>
2022-01-14 12:41:07 +0000

    Initial Arithmetic & if else program

---
+++ src/main/java/org/example/ArithmeticOperators.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args){
+        int A=30;
+        int B=10;
+        byte n=127;
+        long myNum = 1200000000L;
+        short num =6000;
+        int i=20;
+        float f1 = 12.34f;
+        double d = 1233.233;
+        char c = 'S';
+        boolean iLikejava = true;
+        boolean iLikeatesting = false;
+        String s = ("Hello");
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+        System.out.println(iLikejava);
+        System.out.println(iLikeatesting);
+        System.out.println(s);
+        System.out.println(B+n);
+        System.out.println(myNum-num);
+        System.out.println(num*i);
+        System.out.println(f1/d);
+        System.out.println(num%i);
+        System.out.println(++A);
+        System.out.println(--A);
+    }
+}
---
+++ src/main/java/org/example/ifelse.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class ifelse {
+
+    public static void main(String[] args )
+    {
+
+        int x = 18;
+        int y = 15;
+        if(x>y) {
+            System.out.println(x + " is greater than " + y);
+        }
+    }
+}
